506101f6379cad98a4fe9b687a6cd819ef6f93e1
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-05 13:11:59 +0700

    refactor: remove unnecessary utilities dir

--- utils/FilePathIsValid.cs
+++
@@ -1,8 +0,0 @@
-public static class Validation
-{
-
-    public static bool FilePathIsValid(string filePath)
-    {
-        return Directory.Exists(filePath) || filePath.EndsWith(".json");
-    }
-}
