70c740b188c3fd207a00bf4aa2423bae72a6de46
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-06 00:23:26 +0700

    feat: apply JsonFileExists

--- services/JsonFileRegister.cs
+++ services/JsonFileRegister.cs
@@ -4,14 +4,16 @@ namespace model;
 
 public static class JsonFileRegister
 {
-    public static JsonFile RegisterFile(string path)
+    public static JsonFile? RegisterFile(string path)
     {
-        return new JsonFile(path);
+        if (JsonExistsInPath(path)) return new JsonFile(path);
+
+        return null;
     }
 
-    public static List<JsonFile> RegisterFiles(string path)
+    public static List<JsonFile?> RegisterFiles(string path)
     {
-        List<JsonFile> jsonFiles = new List<JsonFile>();
+        List<JsonFile?> jsonFiles = new List<JsonFile?>();
         var files = Directory.GetFiles(path);
         foreach (var file in files)
         {
@@ -19,4 +21,30 @@ public static class JsonFileRegister
         }
         return jsonFiles;
     }
+
+
+    private static bool JsonExistsInPath(string path)
+    {
+        try
+        {
+            if (Path.GetFileName(path).EndsWith(".json") && File.Exists(path)) return true;
+
+
+            var files = Directory.GetFiles(path);
+            if (files.Any(file => file.EndsWith(".json"))) return true;
+
+            throw new FileNotFoundException();
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.WriteLine($"\nDirectory {path} not found\n");
+            return false;
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("\nJSON File not found\n");
+            return false;
+        }
+    }
+
 }
