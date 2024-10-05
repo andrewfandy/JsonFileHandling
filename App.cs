1e66e7728bc9551ea8f620b9e4827b914be6cb18
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-06 00:23:47 +0700

    feat: simplify App

--- App.cs
+++ App.cs
@@ -1,5 +1,7 @@
 
 using model;
+using services;
+using utils;
 
 namespace JsonFileHandling;
 
@@ -15,8 +17,24 @@ public class App
 
     private void Run()
     {
-        string input = InputPath();
-        RegisterJSON(input);
+        Console.WriteLine("INPUT DRAFT FILE");
+        var draftFile = JsonFileRegister.RegisterFile(InputHelper.Input("Input .json file: "));
+
+        Console.WriteLine("\nINPUT CONFIRMED FILE");
+        var confirmedFile = JsonFileRegister.RegisterFile(InputHelper.Input("Input .json file: "));
+
+        var compared = JsonHandlingService.CompareAndMerge(draftFile, confirmedFile);
+
+        if (compared == null)
+        {
+            Console.WriteLine("Compared JObject null");
+            return;
+        }
+        // var matchedDb = JsonHandlingService.CompareFieldsOnDB(compared);
+        foreach (var obj in compared)
+        {
+            Console.WriteLine(obj.ToString());
+        }
 
         Console.WriteLine("\nProcess Ends\nStart again?press 'y' or 'n'");
         ConsoleKey key;
@@ -27,59 +45,13 @@ public class App
             {
                 new App();
             }
-            else
+            else if (key == ConsoleKey.N)
             {
                 Console.WriteLine("Good bye");
-                break;
+                return;
             }
         } while (key != ConsoleKey.Y || key != ConsoleKey.N);
 
     }
-
-    private void RegisterJSON(string path)
-    {
-        var jsonFiles = Directory.Exists(path)
-            ? JsonFileRegister.RegisterFiles(path)
-            : new List<JsonFile> { JsonFileRegister.RegisterFile(path) };
-    }
-
-    private bool JsonExistsInPath(string path)
-    {
-        try
-        {
-            if (Directory.Exists(path) && Path.GetFileName(path).EndsWith("json")) return true;
-            var files = Directory.GetFiles(path);
-
-            if (files.Any(file => file.EndsWith(".json"))) return true;
-
-            throw new FileNotFoundException();
-        }
-        catch (DirectoryNotFoundException)
-        {
-            Console.WriteLine($"Directory {path} not found");
-            return false;
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("JSON File not found");
-            return false;
-        }
-    }
-    private string InputPath()
-    {
-        Console.Write("Input .json file or the path that contains it: ");
-        string? path = Console.ReadLine();
-        if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))
-        {
-            Console.WriteLine("Input must not null or empty spaces\nTry Again please\n");
-            return InputPath();
-        }
-
-        if (!JsonExistsInPath(path))
-        {
-            Console.WriteLine("No JSON file in the path");
-            return InputPath();
-        }
-        return path;
-    }
 }
+
