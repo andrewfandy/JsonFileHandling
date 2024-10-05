e3b8a6bd256d7e99301b2b5dbfbefdce11e63c15
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-05 14:42:02 +0700

    refactor: improvement on better readiness and validation of JSON file

--- App.cs
+++ App.cs
@@ -1,4 +1,5 @@
-using System.Diagnostics;
+
+using model;
 
 namespace JsonFileHandling;
 
@@ -14,51 +15,71 @@ public class App
 
     private void Run()
     {
-        string pathConfirmed = "";
-        string pathDraft = "";
-        while (string.IsNullOrEmpty(pathConfirmed))
-        {
-            Console.WriteLine("Input path of confirmed doc: ");
-            pathConfirmed = Console.ReadLine()!;
-        }
-        while (string.IsNullOrEmpty(pathDraft))
-        {
+        string input = InputPath();
+        RegisterJSON(input);
 
-            Console.WriteLine("Input path of draft doc: ");
-            pathDraft = Console.ReadLine()!;
-        }
-        if (File.Exists(pathConfirmed) && File.Exists(pathDraft))
+        Console.WriteLine("\nProcess Ends\nStart again?press 'y' or 'n'");
+        ConsoleKey key;
+        do
         {
-            var confirmed = JsonFileRegister.RegisterSingleFile(pathConfirmed);
-            var draft = JsonFileRegister.RegisterSingleFile(pathDraft);
-
-            if (confirmed != null && draft != null)
+            key = Console.ReadKey(intercept: true).Key;
+            if (key == ConsoleKey.Y)
             {
-                Console.WriteLine("Processing...");
-                ProcessSingleFile(confirmed, draft);
+                new App();
             }
-        }
-        else if (Directory.Exists(pathConfirmed) && Directory.Exists(pathDraft))
-        {
-            var confirmed = JsonFileRegister.RegisterManyFiles(pathConfirmed);
-            var draft = JsonFileRegister.RegisterManyFiles(pathDraft);
-            if (confirmed != null && draft != null)
+            else
             {
+                Console.WriteLine("Good bye");
+                break;
             }
-        }
-        else
-        {
-            Console.WriteLine("Either the JSON File(s) or Path(s) doesn't exists");
-            Run();
-        }
-    }
-    private void ProcessSingleFile(JsonFile confirmedFile, JsonFile draftFile)
-    {
+        } while (key != ConsoleKey.Y || key != ConsoleKey.N);
 
     }
-    private void ProcessingManyFiles(List<JsonFile> confirmedFiles, List<JsonFile> draftFiles)
+
+    private void RegisterJSON(string path)
     {
-        // foreach()
-        // Console.WriteLine($"Processing {confirmedFile.FilePath} and {draftFile.FilePath}");
+        var jsonFiles = Directory.Exists(path)
+            ? JsonFileRegister.RegisterFiles(path)
+            : new List<JsonFile> { JsonFileRegister.RegisterFile(path) };
+    }
+
+    private bool JsonExistsInPath(string path)
+    {
+        try
+        {
+            if (Directory.Exists(path) && Path.GetFileName(path).EndsWith("json")) return true;
+            var files = Directory.GetFiles(path);
+
+            if (files.Any(file => file.EndsWith(".json"))) return true;
+
+            throw new FileNotFoundException();
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.WriteLine($"Directory {path} not found");
+            return false;
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("JSON File not found");
+            return false;
+        }
+    }
+    private string InputPath()
+    {
+        Console.Write("Input .json file or the path that contains it: ");
+        string? path = Console.ReadLine();
+        if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))
+        {
+            Console.WriteLine("Input must not null or empty spaces\nTry Again please\n");
+            return InputPath();
+        }
+
+        if (!JsonExistsInPath(path))
+        {
+            Console.WriteLine("No JSON file in the path");
+            return InputPath();
+        }
+        return path;
     }
 }
