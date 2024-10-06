3f79ea0d2109a2768ebb8ac4f22fa963c1f35c7d
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-10-06 23:08:37 +0700

    initial commit for JsonDBLoader

---
+++ services/JsonDBLoader.cs
@@ -0,0 +1,31 @@
+namespace services;
+
+public class JsonDBLoader
+{
+    /*
+        TODO:
+    - Implement HttpClient request for testing the processed JSON file
+    - Host : http://localhost:5249
+    - POST users/login
+        body:
+        - user: string@gmail.com
+        - password: string
+        - response: token
+    - Store token locally
+    
+    - iteration_1 start
+    - Manually input policy (EQY: 1, SBX: 2)
+
+    - iteration_2 start
+    - POST certificates/
+        header:
+        - Authorization: Bearer ACCESS_TOKEN
+        body:
+        - PolicyId
+        - CurrencyId
+        - ... any processed JObject's field
+    - iteration_2 end
+    - iteration_1 end
+    
+    */
+}
